0abc47250ca1e27cfdbfe999ec87e10cb4374704
masungil70 <103398786+masungil70@users.noreply.github.com>
2024-09-25 11:38:50 +0900

    Dev (#2)
    * 게시판 컨트롤러 클래스 추가
    
    * Update BoardController.java
    
    * Update BoardController.java

---
+++ src/main/java/org/kosa/hello/board/BoardController.java
@@ -0,0 +1,15 @@
+package org.kosa.hello.board;
+
+import org.springframework.stereotype.Controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@Slf4j
+@RequiredArgsConstructor
+public class BoardController {
+	//1. 경섭이 작업한 것 
+	//2. 경섭이 작업한 것 
+}
+
