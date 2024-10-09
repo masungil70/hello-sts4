5dc9c3d2a9620e732ef39a09823faaf650d4d967
masungil <masungil2@daum.net>
2024-10-09 21:15:27 +0900

    스프링 시큐리티 작업을 위한 설정 및 클래스 생성 1. 실행시 user 계정에 사용될 password 생성 결과 확인 할 것

---
+++ src/main/java/org/kosa/hello/config/CustomSecurityConfig.java
@@ -0,0 +1,13 @@
+package org.kosa.hello.config;
+
+import org.springframework.context.annotation.Configuration;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+public class CustomSecurityConfig {
+
+}
